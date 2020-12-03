d8cdccea403e3387bbd92b829c81e4491cfd5a32
shane-liu-1010 <59184894+shane-liu-1010@users.noreply.github.com>
2020-12-04 00:48:09 +0800

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,184 +1,197 @@
+#include<iostream>
 #include<iomanip>
 #include<cmath>
-class Matrix{
-    private:
-        int _n;
-        int _m;
-        int _arr[100][100];
-        int _maxL;
-    public:
-        Matrix(){}
-        Matrix(int n,int m,int a=0){
-            _n=n;
-            _m=m;
-            _maxL=log10(a)+1;
-            for(int i=1;i<=_n;i++){
-                for(int j=1;j<=_m;j++){
-                    if(i==j)_arr[i][j]=a;
-                    else _arr[i][j]=0;
-                }
-            }
-        }
-        Matrix(int n,int m,int a[100][100]){
-            _n=n;
-            _m=m;
-            _maxL=0;
-            int t=0;
-            for(int i=1;i<=_n;i++){
-                for(int j=1;j<=_m;j++){
-                    _arr[i][j]=a[i-1][j-1];
-                    t=log10(abs(_arr[i][j]))+1;
-                    if(_arr[i][j]<0)t++;
-                    if(t>_maxL)_maxL=t;
-                }
-            }
-        }
-        friend Matrix operator+(Matrix a,Matrix b){
-            if(a._n!=b._n &&a._m!=b._m){
-                cerr<<"two Matrix's size need to be same"<<endl;
-                exit(1);
-            }
-            int c[100][100];
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=a._m;j++){
-                    c[i-1][j-1]=a._arr[i][j]+b._arr[i][j];
-                }
-            }
-            Matrix ans(a._n,a._m,c);
-            return ans;
-        }
-        friend Matrix operator*(Matrix a,Matrix b){
-            if(a._m!=b._n){
-                cerr<<"A's m and B's n need to be same."<<endl;
-                exit(1);
-            }
-            int c[100][100];
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=b._m;j++){
-                    c[i-1][j-1]=0;
-                    for(int z=1;z<=a._m;z++){
-                        c[i-1][j-1]+=a._arr[i][z]*b._arr[z][j];
-                    }
-                }
-            }
-            Matrix ans(a._n,b._m,c);
-            return ans;
-        }
-        friend Matrix operator*(int t,Matrix a){
-            Matrix b(a._n,a._n,t);
-            return b*a;
-        }
-        friend Matrix operator*(Matrix a,int t){return t*a;}
-        friend Matrix operator-(Matrix a,Matrix b){
-            return  a+(-1*b);
-        }
-        friend int det(Matrix a){
-            if(a._n!=a._m){
-                cerr<<"the Matrix must be sqaure"<<endl;
-            }
-            int ans=0;
-            if(a._n==1)ans=a._arr[1][1];
-            else{
-                for(int i=1;i<=a._m;i++){
-                    ans+=int(pow(-1,i+1))*a._arr[1][i]*det(cof(a,1,i));
-                }
-            }
-            return ans;
-        }
-        friend Matrix T(Matrix a){
-            int c[100][100];
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=a._m;j++){
-                    c[j-1][i-1]=a._arr[i][j];
-                }
-            }
-            Matrix ans(a._m,a._n,c);
-            return ans;
-        }
-        friend Matrix cof(Matrix a,int x,int y){
-            int n=a._n-1;
-            int m=a._m-1;
-            int c[100][100];
-            int q=0,w=0;
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=a._m;j++){
-                    if(i!=x && j!=y){
-                        c[q][w]=a._arr[i][j];
-                        w++;
-                    }
-                }
-                if(i!=x){q++;w=0;}
-            }
-            Matrix ans(n,m,c);
-            return ans;
-        }
-        friend Matrix adj(Matrix a){
-            if(a._n!=a._m){
-                cerr<<"the Matrix must be sqaure"<<endl;
-            }
-            int c[100][100];
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=a._m;j++){
-                    c[i-1][j-1]=int(pow(-1,i+j))*det(cof(a,i,j));
-                }
-            }
-            Matrix ans(a._n,a._m,c);
-            return T(ans);
-        }
-        friend ostream & operator<<(ostream &out,const Matrix &a){
-            for(int i=1;i<=a._n;i++){
-                if(i==1 && i==a._n)out<<"[";
-                else if(i==1)out<<"⎡";
-                else if(i==a._n)out<<"⎣";
-                else out<<"⎢";
-                for(int j=1;j<=a._m;j++){
-                    out<<setw(a._maxL)<<a._arr[i][j];
-                    if(j==a._m){
-                        if(i==1 && i==a._n)out<<"]";
-                        else if(i==1)out<<"⎤";
-                        else if(i==a._n)out<<"⎦";
-                        else out<<"⎥";
-                    }
-                    else out<<" ";
-                }
-                out<<endl;
-            }
-            return out;
-        }
-    	friend istream & operator>>(istream &in,Matrix &a){
-            int t;
-            for(int i=1;i<=a._n;i++){
-                for(int j=1;j<=a._m;j++){
-                    in>>a._arr[i][j];
-                    t=log10(abs(a._arr[i][j]))+1;
-                    if(a._arr[i][j]<0)t++;
-                    if(t>a._maxL)a._maxL=t;
-                }
-            }
-            return in;
-        }
-        friend Matrix inverse(Matrix a){
-            int d=det(a);
-            if(!d){
-                cerr<<"det can't be 0."<<endl;
-                exit(1);
-            }
-            return (1/d)*adj(a);
-        }
-        friend Matrix operator^(Matrix a,int t){
-            Matrix b=a;
-            if(t>0)while(--t)b=b*a;
-            else while(t++)b=inverse(b);
-            return b;
-        }
-        friend bool operator==(Matrix a,Matrix b){
-            if(a._n!=b._n || a._m!=b._m)return false;
-            for(int i=1;i<a._n;i++){
-                for(int j=1;j<a._m;j++){
-                    if(a._arr[i][j]!=b._arr[i][j])return false;
-                }
-            }
-            return true;
-        }
-        friend bool operator!=(Matrix a,Matrix b){return !(a==b);}
+#include<vector>
+#include <cassert>
+
+class Matrix:std::vector<std::vector<double>>{
+private:
+	int maxL;
+public:
+	Matrix(int n,int m,double a=0){
+		resize(n);
+		maxL=1;
+		for(int i=0;i<size();i++){
+			at(i).resize(m);
+			if(n<=m)at(i).at(i)=a;
+		}
+	}
+	Matrix(std::vector<std::vector<double>>a){
+		int t;
+		resize(a.size());
+		for(int i=0;i<size();i++){
+			at(i).resize(a[i].size());
+			for(int j=0;j<a[i].size();j++){
+				at(i).at(j)=a[i][j];
+				t=log10(abs(at(i).at(j)))+1;
+				if(at(i).at(j)<0)t++;
+				if(t>maxL)maxL=t;
+			}
+		}
+	}
+	int row()const{return size();}
+	int column()const{return at(0).size();}
+	void operator+=(Matrix other){*this=*this+other;}
+	void operator-=(Matrix other){*this=*this-other;}
+	void operator*=(Matrix other){*this=*this*other;}
+	void operator^=(int times){*this=*this^times;}
+	void print(){
+		for(int i=0;i<row();i++){
+			if(i==0 && i==row()-1)std::cout<<"[";
+			else if(i==0)std::cout<<"⎡";
+			else if(i==row()-1)std::cout<<"⎣";
+			else std::cout<<"⎢";
+			for(int j=0;j<column();j++){
+				std::cout<<std::setw(maxL)<<at(i).at(j);
+				if(j==column()-1){
+					if(i==0 && i==row()-1)std::cout<<"]";
+					else if(i==0)std::cout<<"⎤";
+					else if(i==row()-1)std::cout<<"⎦";
+					else std::cout<<"⎥";
+				}
+				else std::cout<<" ";
+			}
+			std::cout<<std::endl;
+		}
+	}
+	friend std::ostream & operator<<(std::ostream &out,const Matrix &a){
+		for(int i=0;i<a.row();i++){
+			for(int j=0;j<a.column();j++){
+				out<<std::setw(a.maxL)<<a[i][j]<<" \n"[j==a.column()-1];
+			}
+		}
+		return out;
+	}
+	friend std::istream & operator>>(std::istream &in,Matrix &a){
+		int t;
+		for(int i=0;i<a.row();i++){
+			for(int j=0;j<a.column();j++){
+				in>>a[i][j];
+				t=log10(abs(a[i][j]))+1;
+				if(a[i][j]<0)t++;
+				if(t>a.maxL)a.maxL=t;
+			}
+		}
+		return in;
+	}
+	friend int operator==(Matrix a,Matrix b){
+		if(a.row()!=b.row() || a.column()!=b.column())return 0;
+		for(int i=0;i<a.row();i++){
+			for(int j=0;j<a.column();j++){
+				if(a[i][j]!=b[i][j])return -1;
+			}
+		}
+		return 1;
+	}
+	friend bool operator!=(Matrix a,Matrix b){return !(a==b);}
+	friend Matrix operator+(Matrix a,Matrix b){
+		assert(a==b);
+		std::vector<std::vector<double>>c;
+		c.resize(a.row());
+		for(int i=0;i<a.row();i++){
+			c[i].resize(a.column());
+			for(int j=0;j<a.column();j++){
+				c[i][j]=a[i][j]+b[i][j];
+			}
+		}
+		Matrix ans(c);
+		return ans;
+	}
+	friend Matrix operator-(Matrix a,Matrix b){return a+(-1*b);}
+	friend Matrix operator*(Matrix a,double t){
+		Matrix b(a.row(),a.column(),t);
+		return a*b;
+	}
+	friend Matrix operator*(double t,Matrix a){return a*t;}
+	friend Matrix operator*(Matrix a,Matrix b){
+		assert(a==b);
+		std::vector<std::vector<double>>c;
+		c.resize(a.row());
+		for(int i=0;i<a.row();i++){
+			c[i].resize(a.column());
+			for(int j=0;j<a.column();j++){
+				for(int z=0;z<a.column();z++){
+					c[i][j]+=a[i][z]*b[z][j];
+				}
+			}
+		}
+		Matrix ans(c);
+		return ans;
+	}
+	friend Matrix operator^(Matrix a,int t){
+		assert(t>0);
+		Matrix b=a;
+		while(--t)b=b*a;
+		return b;
+	}
+	friend Matrix T(Matrix a){
+		std::vector<std::vector<double>> c;
+		c.resize(a.column());
+		for(int i=0;i<a.column();i++){
+			c[i].resize(a.row());
+			for(int j=0;j<a.row();j++){
+					c[i][j]=a[j][i];
+			}
+		}
+		Matrix ans(c);
+		return ans;
+	}
+	friend Matrix inverse(Matrix a){
+		double d=det(a);
+		assert(d);
+		return (1/d)*adj(a);
+	}
+	friend double det(Matrix a){
+		double ans=0;
+		if(a.row()==1)ans=a[0][0];
+		else for(int i=0;i<a.column();i++){
+				ans+=pow(-1,i)*a[0][i]*det(cof(a,0,i));
+		}
+		return ans;
+	}
+	friend Matrix cof(Matrix a,int x,int y){
+		assert(a.row()==a.column());
+		std::vector<std::vector<double>>c;
+		c.resize(a.row()-1);
+		int q=0,w=0;
+		for(int i=0;i<a.row()-1;i++){
+			c[i].resize(a.column()-1);
+			w=0;
+			if(q==x)q++;
+			for(int j=0;j<a.column()-1;j++){
+				if(w==y)w++;
+				c[i][j]=a[q][w];
+				w++;
+			}
+			q++;
+		}
+		Matrix ans(c);
+		return ans;
+	}
+	friend Matrix adj(Matrix a){
+		assert(a.row()==a.column());
+		std::vector<std::vector<double>>c;
+		c.resize(a.row());
+		for(int i=0;i<a.row();i++){
+			c[i].resize(a.column());
+			for(int j=0;j<a.column();j++){
+				c[i][j]=pow(-1,i+j)*det(cof(a,i,j));
+			}
+		}
+		Matrix ans(c);
+		return T(ans);
+	}
 };
+
+using namespace std;
+
+int main(){
+	int n,m;
+	cin>>n>>m;
+	Matrix a(n,m);
+	Matrix b(n,m);
+	cin>>a>>b;
+	a+=b;
+	a.print();
+	return 0;
+}
